dca4798ded76cc40d30ddc501c3e0b2614ff584d
Roy Hoitink <L.D.Hoitink@uu.nl>
2023-07-13 09:07:49 +0200

    Changed button order, such that leftmost button returns 0

--- pycontrolpanel/gui.py
+++ pycontrolpanel/gui.py
@@ -15,32 +15,43 @@ class MainWindow(QMainWindow):
         self.centralWidget = QWidget()
         self.setCentralWidget(self.centralWidget)
 
-        layout = QGridLayout()
+        self.layout = QGridLayout()
 
         self.com_label = QLabel("Serial port")
         self.com_input = QLineEdit()
         if serial_port is not None:
             self.com_input.setText(serial_port)
-        layout.addWidget(self.com_label, 0, 0)
-        layout.addWidget(self.com_input, 0, 1)
+        self.layout.addWidget(self.com_label, 0, 0)
+        self.layout.addWidget(self.com_input, 0, 1)
         
         self.baudrate_label = QLabel("Baud rate")
         self.baudrate_input = QLineEdit()
         if baud_rate is not None:
             self.baudrate_input.setText(baud_rate)
-        layout.addWidget(self.baudrate_label, 1, 0)
-        layout.addWidget(self.baudrate_input, 1, 1)
+        self.layout.addWidget(self.baudrate_label, 1, 0)
+        self.layout.addWidget(self.baudrate_input, 1, 1)
         
+        self.create_sliders()
 
         self.connect_btn = QPushButton("Connect")
         self.connect_btn.clicked.connect(self.connect_all)
-        layout.addWidget(self.connect_btn, 2, 0)
+        self.layout.addWidget(self.connect_btn, 2, 0)
 
         self.disconnect_btn = QPushButton("Disonnect")
         self.disconnect_btn.clicked.connect(self.disconnect_all)
         self.disconnect_btn.setDisabled(True)
-        layout.addWidget(self.disconnect_btn, 2, 1)
+        self.layout.addWidget(self.disconnect_btn, 2, 1)
 
+        self.quit_btn = QPushButton("Quit")
+        self.quit_btn.clicked.connect(self.close)
+
+        self.layout.addWidget(self.quit_btn)
+        self.centralWidget.setLayout(self.layout)
+
+        if autoconnect == True:
+            self.connect_all()
+
+    def create_sliders(self):
         self.sliders = []
         self.slider_labels = []
         self.slider_values = []
@@ -59,18 +70,9 @@ class MainWindow(QMainWindow):
             self.sliders.append(QSlider(Qt.Orientation.Horizontal))
             self.sliders[-1].setMinimum(0)
             self.sliders[-1].valueChanged.connect(self.slider_values[-1].setNum)
-            layout.addWidget(self.slider_labels[-1], i+3, 0)
-            layout.addWidget(self.sliders[-1], i+3, 1)
-            layout.addWidget(self.slider_values[-1], i+3, 2)
-
-        self.quit_btn = QPushButton("Quit")
-        self.quit_btn.clicked.connect(self.close)
-
-        layout.addWidget(self.quit_btn)
-        self.centralWidget.setLayout(layout)
-
-        if autoconnect == True:
-            self.connect_all()
+            self.layout.addWidget(self.slider_labels[-1], i+3, 0)
+            self.layout.addWidget(self.sliders[-1], i+3, 1)
+            self.layout.addWidget(self.slider_values[-1], i+3, 2)
 
     def connect_all(self):
         self.connect_serial()
@@ -112,11 +114,11 @@ class MainWindow(QMainWindow):
         self.disconnect_btn.setDisabled(True)
 
     def button_parser(self, btn: Button):
-        if btn.button - 1 < len(self.slider_mappings):
-            mmgroup, mmprop, propmin, propmax, propstep = self.slider_mappings[btn.button - 1]
+        if btn.button < len(self.slider_mappings):
+            mmgroup, mmprop, propmin, propmax, propstep = self.slider_mappings[btn.button]
             current_value = float(self.mmcore.get_property(mmgroup, mmprop))
             new_value = np.clip(current_value + btn.direction*propstep, propmin, propmax)
-            self.sliders[btn.button-1].setValue(int(new_value))
+            self.sliders[btn.button].setValue(int(new_value))
             self.mmcore.set_property(mmgroup, mmprop, float(new_value))
             print(mmprop, new_value)
 
--- pycontrolpanel/parser.py
+++ pycontrolpanel/parser.py
@@ -5,6 +5,7 @@ from serial.serialutil import SerialException
 class ButtonParser:
     DIRECTION_MASK = 0b00000010 # this bit is 1 for CW and 0 for CCW
     BUTTON_MASK = 0b000111 # after bit shift by 2 to the right, these bits contain info on the knob that is turned from 1 for the rightmost button to 7 for the leftmost
+    NUM_BUTTONS = 7
 
     def __init__(self, response):
         self.response = response
@@ -13,7 +14,7 @@ class ButtonParser:
             self.direction = 1
         else:
             self.direction = -1
-        self.button = Button(self.direction, int(self.response >> 2 & self.BUTTON_MASK))
+        self.button = Button(self.direction, self.NUM_BUTTONS - int(self.response >> 2 & self.BUTTON_MASK)) # return button, leftmost button is 0 and rightmost button is 6
 
 class ResponseReader(QThread):
     def __init__(self, ser):
